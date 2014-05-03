741bfdbd9de77edf77f2ebc285447a1bf2a15a03
Sergey Ovechkin <me@sovechkin.com>
2014-05-03 16:31:59 +0400

    add dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :application, "api.pomeo.me"
 require           "capistrano-offroad"
 offroad_modules   "defaults", "supervisord"
 set :repository,  "git@github.com:pomeo/#{application}.git"
+set :deploy_to,   "/home/ubuntu/www/api"
 set :supervisord_start_group, "api"
 set :supervisord_stop_group,  "api"
 #========================
