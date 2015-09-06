f4beaf7790244b50050a648909b4ac92838236a2
Sergey Ovechkin <me@pomeo.me>
2015-09-06 23:32:16 +0300

    FIX broken with new version modules

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,38 @@
-var express    = require('express'),
-    router     = express.Router(),
-    winston    = require('winston'),
-    xml2js     = require('xml2js'),
-    moment     = require('moment'),
-    rest       = require('restler'),
-    Twit       = require('twit'),
-    NodeCache  = require('node-cache'),
-    myCache    = new NodeCache({stdTTL: 3600, checkperiod: 120}),
-    Logentries = require('winston-logentries');
+'use strict';
+const express    = require('express');
+const router     = express.Router();
+const winston    = require('winston');
+const moment     = require('moment');
+const rest       = require('restler');
+const _          = require('lodash');
+const Twit       = require('twit');
+const NodeCache  = require('node-cache');
+const myCache    = new NodeCache({
+  stdTTL: 3600, checkperiod: 120
+});
 
-var T = new Twit({
+let T = new Twit({
   consumer_key:        process.env.TWITTER_CONSUMER_KEY,
   consumer_secret:     process.env.TWITTER_CONSUMER_KEY_SECRET,
   access_token:        process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+let logger;
+
 if (process.env.NODE_ENV === 'development') {
-  var logger = new (winston.Logger)({
+  logger = new (winston.Logger)({
     transports: [
       new (winston.transports.Console)()
     ]
   });
 } else {
-  var logger = new (winston.Logger)({
+  require('le_node');
+  logger = new (winston.Logger)({
     transports: [
-      new winston.transports.Logentries({token: process.env.logentries})
+      new winston.transports.Logentries({
+        token: process.env.logentries
+      })
     ]
   });
 }
@@ -37,61 +44,80 @@ router.get('/', function(req, res) {
 router.get('/twitter', function(req, res) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
-  if (Object.getOwnPropertyNames(myCache.get('twitter')).length == 0) {
-    T.get('statuses/user_timeline', {
-      screen_name: 'pomeo',
-      count: 2
-    }, function (err, statuses) {
-         var twArray = [];
-         for (var i = 0; i < statuses.length; i++) {
-           twArray.push({
-             url: 'https://twitter.com/pomeo/statuses/' + statuses[i].id_str,
-             date: moment(new Date(statuses[i].created_at)).fromNow(),
-             content: statuses[i].text
-           });
-         }
-         myCache.set('twitter', twArray);
-         res.json(myCache.get('twitter'));
-       });
-  } else {
-    res.json(myCache.get('twitter'));
-  }
+  myCache.get('github', function(err, value){
+    if (err) {
+      log('Error: ' + err);
+      res.send('err');
+    } else {
+      if (_.isUndefined(value)) {
+        T.get('statuses/user_timeline', {
+          screen_name: 'pomeo',
+          count: 2
+        }, function (err, statuses) {
+          if (err) {
+            log('Error: ' + err);
+            res.send('err');
+          } else {
+            let twArray = [];
+            for (var i = 0; i < statuses.length; i++) {
+              twArray.push({
+                url: 'https://twitter.com/pomeo/statuses/' + statuses[i].id_str,
+                date: moment(new Date(statuses[i].created_at)).fromNow(),
+                content: statuses[i].text
+              });
+            }
+            myCache.set('twitter', twArray);
+            res.json(myCache.get('twitter'));
+          }
+        });
+      } else {
+        res.json(myCache.get('twitter'));
+      }
+    }
+  });
 });
 
 router.get('/github', function(req, res) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
-  if (Object.getOwnPropertyNames(myCache.get('github')).length == 0) {
-    rest.get('https://github.com/pomeo.atom', {
-      parser: rest.parsers.xml
-    }).once('complete', function(result) {
-      if (result instanceof Error) {
-        log('Error:' + result.message, 'error');
-        res.send('ok');
+  myCache.get('github', function(err, value){
+    if (err) {
+      log('Error: ' + err);
+      res.send('err');
+    } else {
+      if (_.isUndefined(value)) {
+        rest.get('https://github.com/pomeo.atom', {
+          parser: rest.parsers.xml
+        }).once('complete', function(result) {
+          if (result instanceof Error) {
+            log('Error:' + result.message, 'error');
+            res.send('ok');
+          } else {
+            let githubArray = [];
+            for (var i = 0; i < 2; i++) {
+              githubArray.push({
+                date: '<time datetime="' + result.feed.entry[i].published[0] + '">' + moment(result.feed.entry[i].published[0]).fromNow() + '</time>',
+                content: result.feed.entry[i].content[0]._.replace(new RegExp('(href\=\")[^http]', 'g'), 'href="https:\/\/github.com\/')
+              });
+            }
+            myCache.set('github', githubArray);
+            res.json(myCache.get('github'));
+          }
+        });
       } else {
-        var githubArray = [];
-        for (var i = 0; i < 2; i++) {
-          githubArray.push({
-            date: '<time datetime="' + result['feed']['entry'][i]['published'] + '">' + moment(result.feed.entry[i].published).fromNow() + '</time>',
-            content: result.feed.entry[i].content[0]._.replace(new RegExp('(href\=\")[^http]', 'g'), 'href="https:\/\/github.com\/')
-          });
-        }
-        myCache.set('github', githubArray);
         res.json(myCache.get('github'));
       }
-    });
-  } else {
-    res.json(myCache.get('github'));
-  }
+    }
+  });
 });
 
 module.exports = router;
 
 function log(logMsg, logType) {
   if (logMsg instanceof Error) logger.error(logMsg.stack);
-  if (logType !== undefined) {
+  if (!_.isUndefined(logType)) {
     logger.log(logType, logMsg);
   } else {
     logger.info(logMsg);
   }
-};
+}
