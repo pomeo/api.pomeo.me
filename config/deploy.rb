f76bc2d1b33b8a840c03500aafcd63140b2dc183
Sergey Ovechkin <me@pomeo.me>
2015-11-23 16:31:59 +0400

    remove change port

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,11 +16,4 @@ set :supervisord_stop_group,  "app"
 set  :gateway,    "#{application}" # main server
 role :app,        "10.3.90.1"      # container
 
-namespace :deploy do
-  desc "Change node.js port"
-  task :chg_port do
-    run "sed -i 's/3000/3700/g' #{current_path}/app.js"
-  end
-end
-
-after "deploy:create_symlink", "deploy:npm_install", "deploy:chg_port", "deploy:restart"
+after "deploy:create_symlink", "deploy:npm_install", "deploy:cleanup", "deploy:restart"
