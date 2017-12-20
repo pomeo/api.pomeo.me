6f10425e108807c49b691e783ef8f5580e49d601
Sergey Ovechkin <me@pomeo.me>
2017-12-20 13:38:38 +0300

    add winston with graylog

--- routes/index.js
+++ routes/index.js
@@ -1,71 +1,16 @@
 'use strict';
 const express    = require('express');
 const router     = express.Router();
-const log        = require('winston-logs')({
-  production: {
-    logentries: {
-      token: process.env.logentries
-    }
-  },
-  development: {
-    'console': {
-      colorize: true
-    }
-  }
-});
 const moment     = require('moment');
 const rest       = require('restler');
-const Twit       = require('twit');
 const NodeCache  = require('node-cache');
 const myCache    = new NodeCache({
   stdTTL: 3600, checkperiod: 120
 });
-
-const T = new Twit({
-  consumer_key:        process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:     process.env.TWITTER_CONSUMER_KEY_SECRET,
-  access_token:        process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+import logger from '../lib/logger';
 
 router.get('/', function(req, res) {
-  res.send('ok');
-});
-
-router.get('/twitter', function(req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
-  myCache.get('twitter', function(err, value){
-    if (err) {
-      log('Error: ' + err);
-      res.send('err');
-    } else {
-      if (value === undefined) {
-        T.get('statuses/user_timeline', {
-          screen_name: 'pomeo',
-          count: 2
-        }, function (err, statuses) {
-          if (err) {
-            log('Error: ' + err);
-            res.send('err');
-          } else {
-            let twArray = [];
-            for (var i = 0; i < statuses.length; i++) {
-              twArray.push({
-                url: 'https://twitter.com/pomeo/statuses/' + statuses[i].id_str,
-                date: moment(new Date(statuses[i].created_at)).fromNow(),
-                content: statuses[i].text
-              });
-            }
-            myCache.set('twitter', twArray);
-            res.json(myCache.get('twitter'));
-          }
-        });
-      } else {
-        res.json(myCache.get('twitter'));
-      }
-    }
-  });
+  res.send('OK');
 });
 
 router.get('/github', function(req, res) {
@@ -73,7 +18,7 @@ router.get('/github', function(req, res) {
   res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
   myCache.get('github', function(err, value){
     if (err) {
-      log('Error: ' + err);
+      logger.error('Error: ' + err);
       res.send('err');
     } else {
       if (value === undefined) {
@@ -81,11 +26,12 @@ router.get('/github', function(req, res) {
           parser: rest.parsers.xml
         }).once('complete', function(result) {
           if (result instanceof Error) {
-            log('Error:' + result.message, 'error');
+            logger.error('Error:' + result.message);
             res.send('ok');
           } else {
             let githubArray = [];
             for (var i = 0; i < 2; i++) {
+              logger.info(result.feed.entry[i].content[0]._);
               githubArray.push({
                 date: '<time datetime="' + result.feed.entry[i].published[0] + '">' + moment(result.feed.entry[i].published[0]).fromNow() + '</time>',
                 content: result.feed.entry[i].content[0]._.replace(new RegExp('(href\=\")[^http]', 'g'), 'href="https:\/\/github.com\/')
---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+const express = require('express');
+const debug = require('debug')('app');
+const path = require('path');
+const favicon = require('serve-favicon');
+const morgan = require('morgan');
+
+let routes = require('./routes/index');
+
+import logger from './lib/logger';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.enable('trust proxy');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    logger.error(err.message);
+    res.sendStatus(500);
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  logger.error(err.message);
+  res.sendStatus(500);
+});
+
+app.listen(app.get('port'), '0.0.0.0', function() {
+  debug('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
